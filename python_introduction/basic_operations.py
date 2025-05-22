bb7608bbaea7134c2797de3743c153481e0c5799
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-05-22 22:51:04 +0100

    initial commit basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+## variable assignment
+number1 = 10
+number2 = 5
+
+# math operation on variables
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# output of operation
+print("Addition of", number1, "and", number2, "is", sum) 
