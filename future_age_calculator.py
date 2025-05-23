d58231240a2d7f691040a6f27f6a38d8aab7e7dc
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-05-24 00:06:15 +0100

    initial commit future_age_calculator.py

---
+++ future_age_calculator.py
@@ -0,0 +1,10 @@
+# collect input from user
+current_age = int(input("How old are you?\n"))
+
+# calculate user age in the future
+current_year = 2023
+future_year = 2050
+future_age = (future_year - current_year) + current_age 
+
+# output of user year in the future
+print("In", future_year, "you will be", future_age, "years old.")
