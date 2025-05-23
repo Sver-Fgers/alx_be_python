eabe0e4b564eceb56039588f1f04736bd2b4db85
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-05-23 23:56:31 +0100

    inital commit hours_to_seconds.py

---
+++ hours_to_seconds.py
@@ -0,0 +1,10 @@
+# time parameters
+hours = 2
+minutes = 60
+
+# conversion of 2 hours to seconds
+seconds = (60 * minutes) * hours
+
+# output of calculation
+print(hours, "hours(s) is", seconds, "seconds" )
+
