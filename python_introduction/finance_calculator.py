b34cc36f618237ef9db73f9a4ea47e1feb006c2f
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-05-24 01:11:16 +0100

    modified variable definitions

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,14 +1,13 @@
 # collect user finance data
-income = int(input("Enter your monthly income:"))
-expenses = int(input("Enter your total monthly expenses:"))
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 # calculate user montly savings
-monthly_savings = income - expenses
+Monthly_savings = monthly_income - monthly_expenses
 
 # calculate user annual savings
-interest = 5
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+Projected_savings = Monthly_savings * 12 + (Monthly_savings * 12 * 0.05)
 
 # output of user financials
-print("Your montly savings are", monthly_savings)
-print("Projected savings after one year, with interest, is:", projected_savings)
+print("Your montly savings are", Monthly_savings)
+print("Projected savings after one year, with interest, is:", Projected_savings)
