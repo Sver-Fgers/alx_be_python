6a50edb8d4c901b0a44c98d48854e00fc792c7b7
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-06-01 23:55:36 +0100

    modified multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -9,4 +9,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 # Print the multiplication table
 for multiplier in range(1, 11):
     product = number * multiplier
-    print(number, "x", multiplier, "=", product)
+    print(number, "*", multiplier, "=", product)
