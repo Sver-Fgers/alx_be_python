c7780df2219a4a5b2e14fb8cb900fd81c2171f09
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-06-21 14:06:21 +0100

    modified method display_balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -21,4 +21,4 @@ class BankAccount:
 
     # display balance prints the current balance in the account
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
