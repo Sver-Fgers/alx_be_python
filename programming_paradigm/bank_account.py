2fdeb748e9fcfd3cdb34f8ee37e7c67f849cb154
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-06-21 12:09:40 +0100

    initial commit - bank account script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+# this script that simulates simple bank account transactions from the command line
+
+class BankAccount:
+
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+
+    # deposit amount adds the specified amount to account_balance
+    def deposit(self, amount):
+        #self.initial_balance += amount
+        self.account_balance += amount
+
+    # withdraw deduct the amount from account balance if funds are sufficient
+    # returning True; otherwise, return False and does not alter the balance
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    # display balance prints the current balance in the account
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance:.2f}")
