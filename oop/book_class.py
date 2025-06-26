84962fd08d16a055c7668e54e29f07dd49621724
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-06-26 17:58:34 +0100

    modified book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,6 +12,6 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book(`{self.title}`, '{self.author}', '{self.author}')"
+        return f"Book('{self.title}', '{self.author}', '{self.author}')"
 
 
