a532ce5ac0c7afd725b1aaedd53d867ca74d941c
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-06-27 15:06:45 +0100

    initial commit - class_static_methods_demo scripts

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    # class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
+    
+    @staticmethod
+    def add(a,b):
+        return a + b
